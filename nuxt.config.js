f98d02d3091c016bc0b563e1ee6a583218fcc78f
Frédéric Ntawiniga <ntfreddy@yahoo.ca>
2020-08-21 21:00:16 -0400

    hello

--- nuxt.config.js
+++ nuxt.config.js
@@ -91,14 +91,7 @@ export default {
     },
     firebase: {
         config: {
-            apiKey: "AIzaSyC6h1NziWg3vXHurmvzaQOfaS0s45a1ob8",
-            authDomain: "better-future-app.firebaseapp.com",
-            databaseURL: "https://better-future-app.firebaseio.com",
-            projectId: "better-future-app",
-            storageBucket: "better-future-app.appspot.com",
-            messagingSenderId: "505959582940",
-            appId: "1:505959582940:web:3acb59a663680903b3b819",
-            measurementId: "G-NTYRRWCG6F"
+
         },
         onFirebaseHosting: true,
         services: {
